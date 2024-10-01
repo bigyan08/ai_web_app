9949193d8017360612c13e730406f29b6e21ab13
Bigyan <bigyan8aryal@gmail.com>
2024-10-01 23:23:10 +0545

    generated content done

--- ai_app/views.py
+++ ai_app/views.py
@@ -5,7 +5,55 @@ from django.contrib.auth import authenticate,login, logout
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+import json
+from pytube import YouTube
+from django.conf import settings
+import os
+import assemblyai as aai
+from dotenv import load_dotenv
+import openai
+
 # Create your views here.
+
+def yt_title(link):
+    yt= YouTube(link)
+    title=yt.title
+    return title
+
+def download_audio(link):
+    yt=YouTube(link)
+    video = yt.streams.filter(only_audio=True).first()
+    out_file = video.download(output_path=settings.MEDIA_ROOT)
+    base,ext= os.path.splitext(out_file) #separating the name of the file to base and ext, i.e is base name and extension name
+    new_file = base + ".mp3" # then creating a new file which has base name but mp3 extension
+    os.rename(out_file,new_file)
+    return new_file
+
+def get_transcription(link):
+    audio_file=download_audio(link)
+    aai.settings.api_key=os.getenv("ASSEMBLY_API_KEY")
+
+    transcriber = aai.Transcriber()
+    transcript=transcriber.transcribe(audio_file)
+    return transcriber.text
+
+def generate_blog_from_transcription(transcription):
+    openai.api_key = os.getenv("OPENAI_KEY")
+    prompt = f'Based on the following transcript from a YouTube video, write a comprehensive blog article, write it based on the transcript, but donot make it look like a youtube video, make it look like a proper blog article: \n\n {transcription}\n\nArticle:'
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1000
+    )
+    generated_content= response.choices[0].text.strip() 
+    # here if it was just response, then it would return in a dictionary format. choices[0] takes first choice, strip() strips the text of the first choice.
+    #response.choices: Accesses the list of choices (possible generated completions).
+    # response.choices[0]: Accesses the first completion (choice) in the list.
+    # response.choices[0].text: Extracts the raw generated text from the first completion.
+    # response.choices[0].text.strip(): Removes any extra spaces, newlines, or tabs from the beginning and end of the generated text.
+    return generated_content
+
+
 
 # decorators= that takes function as an argument and modifies them without changing their functionalities.
 # here, using this decorator only user who is logged in can view this page.
@@ -17,9 +65,24 @@ def index(request):
 @csrf_exempt # in this case, there is no need to use csrf token
 def generate_blog(request):
     if request.method == 'POST':
-        pass
+        try:
+           data= json.loads(request.body) 
+           yt_link = data['link'] 
+        except(KeyError,json.JSONDecodeError):
+            return JsonResponse({'error':'Invalid Data Sent'}, status=400)
+        
+
+        #get title
+        title= yt_title(yt_link)
+
+        #get transcript
+        transcription=get_transcription(yt_link)
+        if not transcription:
+            return JsonResponse({'error':"Transcript process failed!"},status=500)
+
     else:
         return JsonResponse({'error':'Invalid request method'}, status=405) #we dont want user to use GET method.
+    
 
 def user_signup(request):
     if request.method == 'POST':
